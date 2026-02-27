3434721beba7a8db239f0059c97793779076b501
dongjinghk-stack <dongjinghk@gmail.com>
2026-02-27 12:34:59 +0800

    fix [DEVEL-6401] Test Case Failed: [EN] Negative - Product Comparison & Validate the Shopping Cart Pricing

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the ascript.asdfasdf2baadsfkj
+# Press the green button in the gutter to run the ascript.b
 if __name__ == '__main__':
     print_hi('PyCharm')
 
