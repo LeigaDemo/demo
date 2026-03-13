89937e6b6775c17c2747c1b83f43005b74517048
dongjinghk-stack <dongjinghk@gmail.com>
2026-03-13 09:30:16 +0800

    fix [DEVEL-6751] Test Case Failed: [Demo] Create new Lead and Verify - Negative

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.asdf
 
 
-# Press the green button in the gutter to run the ascript.asdfasdaa
+# Press the green button in the gutter to run the ascript.asdfasdaaasdfad
 if __name__ == '__main__':
     print_hi('PyCharm')
 
