096dc0b408c8e257e7bce450f4374f75fb84e4cb
dongjinghk-stack <dongjinghk@gmail.com>
2026-04-29 19:22:08 +0800

    fix [DEVEL-7308] Test Case Failed: [AR] STEP 1: Create Sales Order

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.asdf
 
 
-# Press the green button in the gutter to run the ascript. testing April-27
+# Press the green button in the gutter to run the ascript. testing April-27asdfasas
 if __name__ == '__main__':
     print_hi('PyCharm')
 
